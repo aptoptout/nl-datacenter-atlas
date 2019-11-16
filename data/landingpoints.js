7c63b2b725ce0b95475aa7be6c9faf5753934437
Apt Opt Out <message@aptoptout.com>
2019-11-16 12:44:38 +0100

    :sparkles: added high res export and better styling without HTML elements.

---
+++ data/landingpoints.js
@@ -0,0 +1,81 @@
+const geojson_landingpoints = {
+    "type": "FeatureCollection",
+    "features": [{
+        "type" : "Feature",
+        "geometry" : {
+            "type" : "Point",
+            "coordinates" : [4.613676,52.458517]
+        },
+        "properties" : {
+            "name" : "ijmuiden-netherlands",
+            "city" : "Ijmuiden",
+            "cable_id" : 1936
+        }
+    },{
+        "type" : "Feature",
+        "geometry" : {
+            "type" : "Point",
+            "coordinates" : [6.816019,53.442806]
+        },
+        "properties" : {
+            "name" : "eemshaven-netherlands",
+            "city" : "Eemshaven",
+            "cable_id" : 1904
+        }
+    },{
+        "type" : "Feature",
+        "geometry" : {
+            "type" : "Point",
+            "coordinates" : [3.496160,51.563950]
+        },
+        "properties" : {
+            "name" : "domburg-netherlands",
+            "city" : "Domburg",
+            "cable_id" : 1819
+        }
+    },{
+        "type" : "Feature",
+        "geometry" : {
+            "type" : "Point",
+            "coordinates" : [4.616374,52.616379]
+        },
+        "properties" : {
+            "name" : "egmond-netherlands",
+            "city" : "Egmond",
+            "cable_id" : 1365
+        }
+    },{
+        "type" : "Feature",
+        "geometry" : {
+            "type" : "Point",
+            "coordinates" : [4.656901,52.486313]
+        },
+        "properties" : {
+            "name" : "beverwijk-netherlands",
+            "city" : "Beverwijk",
+            "cable_id" : 1143
+        }
+    },{
+        "type" : "Feature",
+        "geometry" : {
+            "type" : "Point",
+            "coordinates" : [4.396452,52.205173]
+        },
+        "properties" : {
+            "name" : "katwijk-netherlands",
+            "city" : "Katwijk",
+            "cable_id" : 1043
+        }
+    },{
+        "type" : "Feature",
+        "geometry" : {
+            "type" : "Point",
+            "coordinates" : [4.527214,52.370407]
+        },
+        "properties" : {
+            "name" : "zandvoort-netherlands",
+            "city" : "Zandvoort",
+            "cable_id" : 1137
+        }
+    }]
+};
